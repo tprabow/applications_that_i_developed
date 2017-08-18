bf873ad3a8f1df0a2627cb7eba44e268c0284849
Teguh Harry <teguhharryprabowo@gmail.com>
2017-08-18 13:00:17 +0700

    Added employee controller and web.config setup

---
+++ EFDummyCompany/Models/CompanyContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using EFDummyCompany.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EFDummyCompany.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext() : base("name=CompanyContext")
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<WorksOn> WorksOn { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasMany(e => e.WorksOn)
+                .WithRequired(e => e.Employee)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<Department>()
+                .HasMany(e => e.WorksOn)
+                .WithRequired(e => e.Department)
+                .WillCascadeOnDelete(true);
+        }
+
+        }
+}
